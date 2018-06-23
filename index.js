75e96ea2fb9fd615df96d03c71b3a89173cecffa
Devin Ryan <devin.ryan514@gmail.com>
2018-06-23 20:33:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function logShout(string){
   string = string.toUpperCase();
   return console.log(string);
 }
+
+function logWhisper(string){
+  string = string.toLowerCase();
+  return console.log(string);
+}
